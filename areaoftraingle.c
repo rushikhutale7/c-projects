7e533bc7196fcee63db5a45296f617bc4fce7f18
rushikhutale7 <117240483+rushikhutale7@users.noreply.github.com>
2024-03-19 14:47:07 +0530

    Add files via upload

---
+++ areaoftraingle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+float main()
+{
+
+float b,h,res;
+printf("enter value of base-");
+scanf("%f",&b);
+printf("enter the value of height-");
+scanf("%f",&h);
+
+res=(0.5*b*h);
+printf("area of traingle is - %f",res);
+return 0;
+}
