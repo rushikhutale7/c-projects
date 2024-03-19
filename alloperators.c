f74c48cec5542736b65bc73358a7ad789ea7e685
rushikhutale7 <117240483+rushikhutale7@users.noreply.github.com>
2024-03-19 14:46:02 +0530

    Add files via upload

---
+++ alloperators.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,res;
+    printf("\n enter values for a=");
+    scanf("%d",&a);
+    printf("\n enter values for b=");
+    scanf("%d",&b);
+    res=a+b;
+    printf("\n addtion of 2 number is %d",res);
+
+   
+    res=a-b;
+    printf("\n subtraction of 2 number is %d",res);
+
+
+   
+    res=a*b;
+    printf("\n multiplication of 2 number is %d",res);
+
+     
+    res=a/b;
+    printf("\n division of 2 number is %d",res);
+
+    res=a%b;
+    printf("\n modulus of 2 numbers is %d",res);
+
+    
+
+
+}
