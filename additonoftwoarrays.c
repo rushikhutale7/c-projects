2b071e7f7cb7b7a5c27ad96b75eb0c60187541e8
rushikhutale7 <117240483+rushikhutale7@users.noreply.github.com>
2024-03-28 16:49:42 +0530

    Add files via upload

---
+++ additonoftwoarrays.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<conio.h>
+void firstarray(int a[])
+{
+    int i;
+    printf("enter array elements-\n");
+    for(i=0;i<=4;i++)
+    {
+        printf("enter the values at %d location\n",i);
+        scanf("%d",a+i);
+    }
+}
+void firstarrayview(int a[])
+{
+    int i;
+    printf("display first array elements \n ");
+    for(i=0;i<=4;i++)
+    {
+        printf("\t%d",*(a+i));
+    }
+
+}
+
+void secondarray(int b[])
+{
+    int i;
+    printf("\nenter array elements-\n");
+    for(i=0;i<=4;i++)
+    {
+        printf("enter the values at %d location\n",i);
+        scanf("%d",b+i);
+    }
+}
+void secondarrayview(int b[])
+{
+    int i;
+    printf("display second array elements \n ");
+    for(i=0;i<=4;i++)
+    {
+        printf("\t%d",*(b+i));
+    }
+
+}
+void addarray(int a[],int b[],int c[])
+{
+    int i;
+    printf("\naddition of two arrays is-\n");
+    for(i=0;i<=4;i++)
+    {
+        c[i] = a[i] + b[i];
+        printf("\t%d",*(c+i));
+    }
+    printf("\n");
+}
+
+void main()
+{
+    int a[5];
+    int b[5];
+    int c[5];
+    firstarray(a);
+    firstarrayview(a);
+    secondarray(b);
+    secondarrayview(b);
+    addarray(a,b,c);
+
+}
+
